39fc144989d9c01a3fae94f8472efed1e2a6a005
Chon Torres <chon@chontorres.com>
2024-07-04 16:55:54 -0700

    refactored charts
    improved chart refactor
    refactor chart container

--- StepTracker/Charts/WeightDiffBarChart.swift
+++ StepTracker/Charts/WeightDiffBarChart.swift
@@ -12,46 +12,29 @@ struct WeightDiffBarChart: View {
     @State private var rawSelectedDate: Date?
     @State private var selectedDay: Date?
 
-    var chartData: [WeekdayChartData]
+    var chartData: [DateValueChartData]
 
-    var selectedData: WeekdayChartData? {
-        guard let rawSelectedDate else { return nil }
-        return chartData.first {
-            Calendar.current.isDate(rawSelectedDate, inSameDayAs: $0.date)
-        }
+    var selectedData: DateValueChartData? {
+        ChartHelper.parseSelectedData(from: chartData, in: rawSelectedDate)
     }
 
     var body: some View {
-        VStack {
-            HStack {
-                VStack(alignment: .leading) {
-                    Label("Average Weight Change", systemImage: "figure")
-                        .font(.title3.bold())
-                        .foregroundStyle(.indigo)
+        let chartConfig = ChartContainerConfiguration(title: "Average Weight Change",
+                                                      symbol: "figure",
+                                                      subtitle: "Per Weekday (Last 28 Days)",
+                                                      context: .weight,
+                                                      isNav: false)
 
-                    Text("Per Weekday (Last 28 Days)")
-                        .font(.caption)
-                }
-
-                Spacer()
-            }
-            .foregroundStyle(.secondary)
-            .padding(.bottom, 12)
-
+        ChartContainer(config: chartConfig) {
             if chartData.isEmpty {
                 ChartEmptyView(systemImageName: "chart.bar", title: "No Data",
                                description: "There is no weight data from the Health App")
             } else {
                 Chart {
                     if let selectedData {
-                        RuleMark(x: .value("Selected Data", selectedData.date, unit: .day))
-                            .foregroundStyle(Color.secondary.opacity(0.3))
-                            .offset(y: -10)
-                            .annotation(position: .top,
-                                        spacing: 0,
-                                        overflowResolution: .init(x: .fit(to: .chart), y: .disabled)) { annotationView }
+                        ChartAnnotationView(data: selectedData, context: .weight)
                     }
-
+                    
                     ForEach(chartData) { weightDiff in
                         BarMark(
                             x: .value("Date", weightDiff.date, unit: .day),
@@ -77,33 +60,8 @@ struct WeightDiffBarChart: View {
                 }
             }
         }
-        .padding()
-        .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
     }
 
-    var annotationView: some View {
-        VStack(alignment: .leading) {
-            Text(selectedData?.date ?? .now, format: .dateTime.weekday(.abbreviated).month(.abbreviated).day())
-                .font(.footnote.bold())
-                .foregroundStyle(.secondary)
-
-            Text(selectedData?.value ?? 0, format: .number.precision(.fractionLength(2)))
-                .fontWeight(.heavy)
-                .foregroundStyle((selectedData?.value ?? 0) >= 0 ? .indigo : .mint)
-        }
-        .padding(12)
-        .sensoryFeedback(.selection, trigger: selectedDay)
-        .onChange(of: rawSelectedDate) { oldValue, newValue in
-            if oldValue?.weekdayInt != newValue?.weekdayInt {
-                selectedDay = newValue
-            }
-        }
-        .background(
-            RoundedRectangle(cornerRadius: 4)
-                .fill(Color(.secondarySystemBackground))
-                .shadow(color: .secondary.opacity(0.3), radius: 2, x: 2, y: 2)
-        )
-    }
 }
 
 #Preview {
--- StepTracker/Screens/DashboardView.swift
+++ StepTracker/Screens/DashboardView.swift
@@ -45,10 +45,10 @@ struct DashboardView: View {
 
                     switch selectedStat {
                     case .steps:
-                        StepBarChart(selectedStat: selectedStat, chartData: hkManager.stepData)
+                        StepBarChart(chartData: ChartHelper.convertData(data: hkManager.stepData))
                         StepPieChart(chartData: ChartMath.averageWeekdayCount(for: hkManager.stepData))
                     case .weight:
-                        WeightLineChart(selectedStat: selectedStat, chartData: hkManager.weightData)
+                        WeightLineChart(chartData:ChartHelper.convertData(data: hkManager.weightData))
                         WeightDiffBarChart(chartData: ChartMath.averageDailyWeightDiffs(for: hkManager.weightDiffData))
                     }
                 }
