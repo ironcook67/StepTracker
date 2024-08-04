a5996680cff4c22032375d844250d246b1bf899c
Chon Torres <chon@chontorres.com>
2024-08-03 19:08:27 -0700

    HealthKitManager documentation

--- StepTracker/Managers/HealthKitManager.swift
+++ StepTracker/Managers/HealthKitManager.swift
@@ -18,12 +18,15 @@ import Observation
     var weightData: [HealthMetric] = []
     var weightDiffData: [HealthMetric] = []
 
-    func fetchStepCount(daysBack: Int = 28) async throws -> [HealthMetric] {
+    
+    /// Fetch last 28 days of step count fron HealthKit.
+    /// - Returns: Array of HealthMetric
+    func fetchStepCount() async throws -> [HealthMetric] {
         guard store.authorizationStatus(for: HKQuantityType(.stepCount)) != .notDetermined else {
             throw STError.authNotDetermined
         }
 
-        let interval = createDateInterval(from: .now, daysBack: daysBack)
+        let interval = createDateInterval(from: .now, daysBack: 28)
         let queryPredicate = HKQuery.predicateForSamples(withStart: interval.start, end: interval.end)
         let samplePredicate = HKSamplePredicate.quantitySample(type: HKQuantityType(.stepCount), predicate: queryPredicate)
         let stepsQuery = HKStatisticsCollectionQueryDescriptor(predicate: samplePredicate,
@@ -41,7 +44,10 @@ import Observation
             throw STError.unableToCompleteRequest
         }
     }
-
+    
+    /// Fetch most receent weight sample on each day for a specified number of days back from today.
+    /// - Parameter daysBack: Days back from today. Ex - 28 will return the last 28 days.
+    /// - Returns: ``HealthMetric``
     func fetchWeights(daysBack: Int) async throws -> [HealthMetric] {
         guard store.authorizationStatus(for: HKQuantityType(.bodyMass)) != .notDetermined else {
             throw STError.authNotDetermined
@@ -67,6 +73,10 @@ import Observation
         }
     }
     
+    /// Write step count data to HealthKit. Requires HealthKit write permission.
+    /// - Parameters:
+    ///   - date: Date for step count value.
+    ///   - value: Step count value.
     func addStepData(for date: Date, value: Double) async throws {
         let status =  store.authorizationStatus(for: HKQuantityType(.stepCount))
         switch status {
@@ -88,7 +98,11 @@ import Observation
             throw STError.unableToCompleteRequest
         }
     }
-
+    
+    /// Write weight value to HealthKit. Requires HealthKit write permission.
+    /// - Parameters:
+    ///   - date: Date for weight value.
+    ///   - value: Weight value in pounds. Uses pounds as a Double for .bodyMass conversions.
     func addweightData(for date: Date, value: Double) async throws {
         let status =  store.authorizationStatus(for: HKQuantityType(.bodyMass))
         switch status {
@@ -111,7 +125,12 @@ import Observation
             throw STError.unableToCompleteRequest
         }
     }
-
+    
+    /// Creates a date interval between two dates.
+    /// - Parameters:
+    ///   - date: End of date interval. Ex - today.
+    ///   - daysBack: Start of date interval.. Ex - 28 days ago.
+    /// - Returns: Date range between two dats as a DateInterval.
     private func createDateInterval(from date: Date, daysBack: Int) -> DateInterval {
         let calendar = Calendar.current
         let startOfEndDate = calendar.startOfDay(for: date)
@@ -120,24 +139,24 @@ import Observation
         return .init(start: startDate, end: endDate)
     }
 
-    func addSimulatorData() async {
-        var mockSamples: [HKQuantitySample] = []
-
-        for i in 0..<29 {
-            let stepQuantity = HKQuantity(unit: .count(), doubleValue: .random(in: 4_000...20_000))
-            let weightQuanity = HKQuantity(unit: .pound(), doubleValue: .random(in: (160 + Double(i/3)...165 + Double(i/3))))
-
-            let startDate = Calendar.current.date(byAdding: .day, value: -i, to: .now)!
-            let endDate = Calendar.current.date(byAdding: .second, value: 1, to: startDate)!
-
-            let stepSample = HKQuantitySample(type: HKQuantityType(.stepCount), quantity: stepQuantity, start: startDate, end: endDate)
-            let weightSample = HKQuantitySample(type: HKQuantityType(.bodyMass), quantity: weightQuanity, start: startDate, end: endDate)
-
-            mockSamples.append(stepSample)
-            mockSamples.append(weightSample)
-        }
-
-        try! await store.save(mockSamples)
-        print("✅ Dummy Data sent up")
-    }
+//    func addSimulatorData() async {
+//        var mockSamples: [HKQuantitySample] = []
+//
+//        for i in 0..<29 {
+//            let stepQuantity = HKQuantity(unit: .count(), doubleValue: .random(in: 4_000...20_000))
+//            let weightQuanity = HKQuantity(unit: .pound(), doubleValue: .random(in: (160 + Double(i/3)...165 + Double(i/3))))
+//
+//            let startDate = Calendar.current.date(byAdding: .day, value: -i, to: .now)!
+//            let endDate = Calendar.current.date(byAdding: .second, value: 1, to: startDate)!
+//
+//            let stepSample = HKQuantitySample(type: HKQuantityType(.stepCount), quantity: stepQuantity, start: startDate, end: endDate)
+//            let weightSample = HKQuantitySample(type: HKQuantityType(.bodyMass), quantity: weightQuanity, start: startDate, end: endDate)
+//
+//            mockSamples.append(stepSample)
+//            mockSamples.append(weightSample)
+//        }
+//
+//        try! await store.save(mockSamples)
+//        print("✅ Dummy Data sent up")
+//    }
 }
